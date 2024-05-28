2c087d9c1d32dada1860c9b30f58670bbee18d51
Gustavo Chitangua <gchitangua@gmail.com>
2024-05-28 19:17:00 +0100

    exemploApp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Exemplo from "./componentes/Exemplo/Exemplo";
 
 function App() {
+  // Esse é o componente principal do aplicativo. As paginas,componentes e estrutura principais serem colocadas aqui.
+  //Depois vamos entender melhor
   // Aqui é para estado e javascript
 
   // Aqui no return é onde se coloca a estrutura(componentes, paginas), o html e as classes do css ou até inline style
