7c97d9a85f3eabfd2ac690735d98d6f324278fd4
Gustavo Chitangua <gchitangua@gmail.com>
2024-05-28 18:55:27 +0100

    exemploParaCriar

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import Exemplo from "./componentes/Exemplo/Exemplo";
 
+function App() {
+  // Aqui é para estado e javascript
+
+  // Aqui no return é onde se coloca a estrutura(componentes, paginas), o html e as classes do css ou até inline style
+  return (
+    <div>
+      <Exemplo />
+    </div>
+  );
+}
+
+export default App;
