1ab25a1a0b86f16dba6fcc7749989bb5bf0d6bc1
Gustavo Chitangua <gchitangua@gmail.com>
2024-06-23 21:17:17 +0100

    frontend-ready

--- soga-cliente/src/componentes/eventos/Evento.jsx
+++ soga-cliente/src/componentes/eventos/Evento.jsx
@@ -1,5 +1,86 @@
+import React from "react";
+import style from "./Evento.module.css";
+
 function Evento() {
-  return <div>Evento</div>;
+  return (
+    <div className={style.in}>
+      <div className={style.conteiner}>
+        <div className={style.main}>
+          <h2>Descobrir eventos</h2>
+
+          <div className={style.dividirEvento}>
+            <div className={style.event}>
+              <img
+                src="https://i.guim.co.uk/img/media/9afb9dd3089f1390a0913ea246ea215f035bde5b/0_207_5472_3283/master/5472.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=aea2d09416a408454c070de1ba13444b"
+                alt="Entrega da taça UCL ao Manchester City"
+              />
+              <div className={style.content}>
+                <h3>Entrega da taça UCL </h3>
+
+                <div className={style.descricaoEve1}>
+                  <p>Sex, 21/06 às 19:00</p>
+                  <p>Madrid-Santiago Bernabeu</p>
+                  <p>163 com interesse </p>
+                </div>
+                <button className={style.btn}>Interessado</button>
+              </div>
+            </div>
+
+            <div className={style.evento}>
+              <img
+                src="https://i.guim.co.uk/img/media/9afb9dd3089f1390a0913ea246ea215f035bde5b/0_207_5472_3283/master/5472.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=aea2d09416a408454c070de1ba13444b"
+                alt="Estrutura"
+              />
+              <div className={style.content}>
+                <h3>Estrutura</h3>
+
+                <div className={style.descricaoEve1}>
+                  <p>Seg, 8/07 às 00:00</p>
+                  <p>Luanda</p>
+                  <p>18 com interesse</p>
+                </div>
+                <button className={style.btn}>Interessado</button>
+              </div>
+            </div>
+
+            <div className={style.evento}>
+              <img
+                src="https://i.guim.co.uk/img/media/9afb9dd3089f1390a0913ea246ea215f035bde5b/0_207_5472_3283/master/5472.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=aea2d09416a408454c070de1ba13444b"
+                alt="Estrutura"
+              />
+              <div className={style.content}>
+                <h3>Estrutura</h3>
+
+                <div className={style.descricaoEve1}>
+                  <p>Seg, 8/07 às 00:00</p>
+                  <p>Luanda</p>
+                  <p>18 com interesse</p>
+                </div>
+                <button className={style.btn}>Interessado</button>
+              </div>
+            </div>
+
+            <div className={style.evento}>
+              <img
+                src="https://i.guim.co.uk/img/media/9afb9dd3089f1390a0913ea246ea215f035bde5b/0_207_5472_3283/master/5472.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=aea2d09416a408454c070de1ba13444b"
+                alt="Estrutura"
+              />
+              <div className={style.content}>
+                <h3>Estrutura</h3>
+
+                <div className={style.descricaoEve1}>
+                  <p>Seg, 8/07 às 00:00</p>
+                  <p>Luanda</p>
+                  <p>18 com interesse</p>
+                </div>
+                <button className={style.btn}>Interessado</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Evento;
--- soga-cliente/src/componentes/mensagem/Mensagem.jsx
+++ soga-cliente/src/componentes/mensagem/Mensagem.jsx
@@ -1,5 +1,119 @@
+import { AiOutlineSend } from "react-icons/ai";
+import { TiThMenuOutline } from "react-icons/ti";
+import { MdOutlineManageSearch } from "react-icons/md";
+import style from "./Mensagem.module.css";
+
 function Mensagem() {
-  return <div>Mensagem</div>;
+  return (
+    <div className={style.pageContainer}>
+      <div className={style.outerContainer}>
+        <div className={style.userSidebar}>
+          <div className={style.sidebarHeader}>
+            <TiThMenuOutline className={style.menuIcon} />
+            <MdOutlineManageSearch className={style.searchIcon} />
+          </div>
+          <div className={style.onlineFriends}>
+            <h3>Conversas</h3>
+            <div className={style.friend}>
+              <img
+                src="https://via.placeholder.com/40"
+                alt="Severino"
+                className={style.profileImage}
+              />
+              <div>
+                <span>Mr_Seven</span>
+                <small className={style.timestamp}>Hoje, 10:30 AM</small>
+              </div>
+            </div>
+            <div className={style.friend}>
+              <img
+                src="https://via.placeholder.com/40"
+                alt="Gustavo"
+                className={style.profileImage}
+              />
+              <div>
+                <span>Gustavo Alex</span>
+                <small className={style.timestamp}>Hoje, 10:32 AM</small>
+              </div>
+            </div>
+            <div className={style.friend}>
+              <img
+                src="https://via.placeholder.com/40"
+                alt="Elisama"
+                className={style.profileImage}
+              />
+              <div>
+                <span>Elisama</span>
+                <small className={style.timestamp}>Hoje, 10:30 AM</small>
+              </div>
+            </div>
+            <div className={style.friend}>
+              <img
+                src="https://via.placeholder.com/40"
+                alt="Jeremias"
+                className={style.profileImage}
+              />
+              <div>
+                <span>Jeremias</span>
+                <small className={style.timestamp}>Hoje, 10:30 AM</small>
+              </div>
+            </div>
+          </div>
+          <div className={style.groupChats}>
+            <h3>Grupos</h3>
+            <div className={style.group}>
+              <img
+                src="https://via.placeholder.com/40"
+                alt="Grupo 1"
+                className={style.profileImage}
+              />
+              <div>
+                <span>SOGA - IGF M1</span>
+                <small className={style.timestamp}>Ontem, 9:15 PM</small>
+              </div>
+            </div>
+            <div className={style.group}>
+              <img
+                src="https://via.placeholder.com/40"
+                alt="Grupo 2"
+                className={style.profileImage}
+              />
+              <div>
+                <span>ISAF - IGF 3º Ano</span>
+                <small className={style.timestamp}>Ontem, 8:45 PM</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.chatBox}>
+          <div className={style.chatHeader}>
+            <TiThMenuOutline className={style.menuIcon} />
+            <span>Chat</span>
+            <MdOutlineManageSearch className={style.searchIcon} />
+          </div>
+          <div className={style.chatMessages}>
+            <div className={style.message}>
+              <span className={style.messageContent}>Olá! Como vai?</span>
+              <small className={style.messageTimestamp}>Hoje, 10:30 AM</small>
+            </div>
+            <div className={style.message}>
+              <span className={style.messageContent}>Estou bem, e você?</span>
+              <small className={style.messageTimestamp}>Hoje, 10:32 AM</small>
+            </div>
+          </div>
+          <div className={style.chatInput}>
+            <textarea
+              className={style.messageInput}
+              placeholder="Digite sua mensagem..."
+            />
+            <button className={style.sendButton}>
+              <AiOutlineSend size={25} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mensagem;
